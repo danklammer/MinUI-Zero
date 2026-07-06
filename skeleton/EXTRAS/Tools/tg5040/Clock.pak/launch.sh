f300bba5331d7e7539df61b8ebfc933879d41343
Dan Klammer <danklammer@gmail.com>
2026-07-06 15:24:25 -0400

    Clock tool: the status/toggle UI (the file the last commit claimed to add)

--- skeleton/EXTRAS/Tools/tg5040/Clock.pak/launch.sh
+++ skeleton/EXTRAS/Tools/tg5040/Clock.pak/launch.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+# Clock: set the time, and (opt-in) show HH:MM in the menu next to the battery.
 
 cd $(dirname "$0")
-./clock.elf # &> ./log.txt
+FLAG="$SHARED_USERDATA_PATH/show-clock"
+
+while :; do
+	if [ -f "$FLAG" ]; then STATE="on"; TOGGLE="HIDE CLOCK"; else STATE="off"; TOGGLE="SHOW CLOCK"; fi
+	confirm.elf "Clock
+
+Menu clock: $STATE" "SET TIME" "BACK" "$TOGGLE"
+	RC=$?
+	if [ "$RC" = "0" ]; then
+		./clock.elf
+	elif [ "$RC" = "2" ]; then
+		if [ -f "$FLAG" ]; then rm -f "$FLAG"; else touch "$FLAG"; fi
+		sync
+	else
+		exit 0
+	fi
+done
