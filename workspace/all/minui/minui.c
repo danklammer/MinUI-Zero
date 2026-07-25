cecb642eda3da9258f6fea6290bd560fbf2364b3
Dan Klammer <danklammer@gmail.com>
2026-07-25 04:32:53 -0400

    Enable the built-in boot timing probes (temporary, for the boot-speed work)

--- workspace/all/minui/minui.c
+++ workspace/all/minui/minui.c
@@ -1380,9 +1380,9 @@ static void ChargingScreen(SDL_Surface* screen) {
 }
 
 int main (int argc, char *argv[]) {
-	// LOG_info("time from launch to:\n");
-	// unsigned long main_begin = SDL_GetTicks();
-	// unsigned long first_draw = 0;
+	LOG_info("time from launch to:\n");
+	unsigned long main_begin = SDL_GetTicks();
+	unsigned long first_draw = 0;
 	
 	if (autoResume()) return 0; // nothing to do
 	
@@ -1392,19 +1392,19 @@ int main (int argc, char *argv[]) {
 	InitSettings();
 	
 	SDL_Surface* screen = GFX_init(MODE_MAIN);
-	// LOG_info("- graphics init: %lu\n", SDL_GetTicks() - main_begin);
+	LOG_info("- graphics init: %lu\n", SDL_GetTicks() - main_begin);
 	
 	PAD_init();
-	// LOG_info("- input init: %lu\n", SDL_GetTicks() - main_begin);
+	LOG_info("- input init: %lu\n", SDL_GetTicks() - main_begin);
 	
 	PWR_init();
 	if (!HAS_POWER_BUTTON && !simple_mode) PWR_disableSleep();
-	// LOG_info("- power init: %lu\n", SDL_GetTicks() - main_begin);
+	LOG_info("- power init: %lu\n", SDL_GetTicks() - main_begin);
 	
 	SDL_Surface* version = NULL;
 	
 	Menu_init();
-	// LOG_info("- menu init: %lu\n", SDL_GetTicks() - main_begin);
+	LOG_info("- menu init: %lu\n", SDL_GetTicks() - main_begin);
 	
 	// now that (most of) the heavy lifting is done, take a load off
 	PWR_setCPUSpeed(CPU_SPEED_MENU);
@@ -1416,7 +1416,7 @@ int main (int argc, char *argv[]) {
 	int show_setting = 0; // 1=brightness,2=volume
 	int was_online = PLAT_isOnline();
 	
-	// LOG_info("- loop start: %lu\n", SDL_GetTicks() - main_begin);
+	LOG_info("- loop start: %lu\n", SDL_GetTicks() - main_begin);
 	while (!quit) {
 		GFX_startFrame();
 		unsigned long now = SDL_GetTicks();
@@ -1794,10 +1794,10 @@ int main (int argc, char *argv[]) {
 		}
 		else GFX_sync();
 		
-		// if (!first_draw) {
-		// 	first_draw = SDL_GetTicks();
-		// 	LOG_info("- first draw: %lu\n", first_draw - main_begin);
-		// }
+		if (!first_draw) {
+			first_draw = SDL_GetTicks();
+			LOG_info("- first draw: %lu\n", first_draw - main_begin);
+		}
 		
 		// handle HDMI change
 		static int had_hdmi = -1;
